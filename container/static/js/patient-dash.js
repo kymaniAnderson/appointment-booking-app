ec24057262c58b7ad8a57030ce40bee715920951
Kymani Anderson <kymanianderson@pop-os.localdomain>
2021-09-07 22:04:26 -0500

    patient-dash.js milestone 📍

--- container/static/js/patient-dash.js
+++ container/static/js/patient-dash.js
@@ -1,27 +1,68 @@
 // VARIABLE DECLARATIONS:
 var connectionURL = "http://192.168.100.243:3000";
+var userID;
+var userType;
 
 window.onload = function () {
-    //data = sessionStorage.getItem("data");
-    getSessionData();
+    userID = sessionStorage.getItem("user_id");
+    userType = sessionStorage.getItem("user_type");
+
+    drawTable();
 };
 
-// VIEW SESSION: 
-function getUserSession() {
-    return fetch(connectionURL.concat("/book-appointment"))
+function getAppointments() {
+    return fetch(connectionURL.concat("/personal-appointment/").concat(userID))
         .then((res) => res.json())
         .then((json) => json);
 }
 
-async function getSessionData() {
-    let data = await getUserSession();
-    console.log(data);
+async function drawTable() {
+    let appointments = await getAppointments();
+
+    appointments.forEach((appointment) => {
+        addTableBody(appointment);
+    });
+    addTableHead();
+}
+
+
+// POPULATE TABLE:
+function addTableBody(appointment) {
+    var table = document.getElementById("appointment-table");
+    var row = table.insertRow(0);
+
+    var appointmentID = row.insertCell(0);
+    var appointmentDate = row.insertCell(1);
+    var appointmentTime = row.insertCell(2);
+    var appointmentReason = row.insertCell(3);
+    var appointmentStatus = row.insertCell(3);
+
+    appointmentID.innerHTML = appointment._id;
+    appointmentDate.innerHTML = appointment.appointment_date;
+    appointmentTime.innerHTML = appointment.appointment_time;
+    appointmentReason.innerHTML = appointment.appointment_reason;
+    appointmentStatus.innerHTML = appointment.appointment_status;
+}
+
+function addTableHead() {
+    var table = document.getElementById("appointment-table");
+    var header = table.createTHead();
+    var row = header.insertRow(0);
+
+    var appointmentIDHead = row.insertCell(0);
+    var appointmentDateHead = row.insertCell(1);
+    var appointmentTimeHead = row.insertCell(2);
+    var appointmentReasonHead = row.insertCell(3);
+    var appointmentStatusHead = row.insertCell(3);
+
+    appointmentIDHead.innerHTML = "Appointment ID";
+    appointmentDateHead.innerHTML = "Date";
+    appointmentTimeHead.innerHTML = "Time";
+    appointmentReasonHead.innerHTML = "Reason";
+    appointmentStatusHead.innerHTML = "Status";
 }
 
 function logout() {
-    return fetch(connectionURL.concat("/logout"))
-        .then((res) => res.json())
-        .then(function () {
-            window.location.href = "login.html"
-        });
+    sessionStorage.clear();
+    window.location.href = "login.html"
 }
